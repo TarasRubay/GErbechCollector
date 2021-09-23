ef9faa50ae10969394c38a621cab7048cd90aa60
TarasRubay <84627325+TarasRubay@users.noreply.github.com>
2021-09-23 20:18:48 +0300

    Add project files.

---
+++ GErbechCollector/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace GErbechCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = null;
+            //Random random = new Random();
+            //Console.WriteLine(GC.MaxGeneration);
+            //Console.WriteLine(GC.GetGeneration(random));
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    str = new String('*',random.Next(1,10));
+            //    // Console.WriteLine(GC.GetGeneration(str));
+            //    //GC.Collect();
+            //}
+            //foreach (var item in str)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine(GC.GetGeneration(random));
+
+
+
+            //using (UIntArray array = new UIntArray(100))
+            //{
+            //    for (int i = 0; i < array.Lengs; i++)
+            //    {
+            //        //array[i] = 0;
+            //        Console.Write($"{array[i]} ");
+            //    }
+
+            //}
+
+
+            IntPtr hwnd = ExternalMethods.GetConsoleWindow();
+           IntPtr hdc = ExternalMethods.GetDC(hwnd);
+            using (Graphics g = Graphics.FromHdc(hdc))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.DrawEllipse(new Pen(Brushes.Blue, 10), 10, 10, 300, 400);
+                g.FillEllipse(Brushes.Yellow, 10, 10, 300, 400);
+               
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ GErbechCollector/UIntArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; /// 
+
+
+namespace GErbechCollector
+{
+    unsafe public class UIntArray : IDisposable
+    {
+        bool isDispouset;
+        int* array = null;
+        public int Lengs { get; set; }
+        public UIntArray(int length)
+        {
+            Lengs = length;
+            array = (int*)Marshal.AllocHGlobal(Lengs * Marshal.SizeOf<int>());
+            GC.AddMemoryPressure(Lengs * Marshal.SizeOf<int>());
+        }
+        public int this[int index] {
+                get => array[index];
+                set => array[index] = value;
+                }
+        ~UIntArray() {
+            Clean(false);
+        }
+
+        protected void Clean(bool userDispouse) {
+            if (!isDispouset) {
+                Marshal.FreeHGlobal((IntPtr)array);
+                GC.RemoveMemoryPressure(Lengs * sizeof(int));
+                if (userDispouse) {
+                    Lengs = 0;
+                
+                }
+             
+
+                isDispouset = true;
+            }
+        }
+        public void Dispose()
+        {
+            Clean(true);
+            GC.SuppressFinalize(this);
+            
+        }
+    }
+}
